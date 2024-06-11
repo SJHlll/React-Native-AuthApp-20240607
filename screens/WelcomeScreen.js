16f9e7b8fc59520e10b40006e56a9436fa264ed2
SJHlll <ssaadd1212@gmail.com>
2024-06-11 11:50:29 +0900

    #7. Todo 관련 컴포넌트 추가, 비동기 통신 설정

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native';
+import Button from '../components/ui/Button';
+import { useNavigation } from '@react-navigation/native';
 
 function WelcomeScreen() {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Button
+        title='Go to Todo List'
+        onPress={() => navigation.navigate('todoTemplate')}
+      />
     </View>
   );
 }
